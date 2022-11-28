6bf298697da8c5488b27af75208e0433dd28de38
Jesper Eriksson <jesper@eriksson.studio>
2022-11-28 11:33:39 +0100

    👣: add support for telia (#1)
    * 👣: add support for telia
    
    Co-authored-by: Hannes L <734718+carlhannes@users.noreply.github.com>

--- index.js
+++ index.js
@@ -20,15 +20,27 @@ const getConfig = async () => {
   }
 };
 
+const objectToUpperCase = async (obj) => Object.keys(obj).reduce((accumulator, key) => {
+  accumulator[key.toUpperCase()] = obj[key];
+  return accumulator;
+}, {});
+
 // main program
-const rebootDevice = async ({ browser, target, username, password }) => {
+const rebootDevice = async ({
+  browser,
+  TARGET,
+  USERNAME,
+  PASSWORD,
+  DRYRUN = false,
+}) => {
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  await page.goto(target || 'http://192.168.1.1/');
+  await page.goto(TARGET || 'http://192.168.1.1/');
 
   // get first applicable username field
   const usernameFld = await Promise.any([
+    page.getByPlaceholder('User', { timeout: 1000 }),
     page.getByPlaceholder('Username', { timeout: 1000 }),
     page.getByText('Username', { timeout: 1000 }),
     page.getByLabel('Username', { timeout: 1000 }),
@@ -40,9 +52,8 @@ const rebootDevice = async ({ browser, target, username, password }) => {
   ]).catch(() => {
     console.log('No username field found');
   });
-
   if (usernameFld) {
-    await usernameFld.fill(username || 'admin');
+    await usernameFld.fill(USERNAME || 'admin');
   }
 
   // get the first applicable password field
@@ -51,55 +62,81 @@ const rebootDevice = async ({ browser, target, username, password }) => {
     page.getByText('Password', { timeout: 1000 }),
     page.getByLabel('Password', { timeout: 1000 }),
     page.locator('input[type="password"]', { timeout: 1000 }),
-    page.locator('input[name="password"]', { timeout: 1000 })
+    page.locator('input[name="password"]', { timeout: 1000 }),
   ]).catch(() => {
     throw new Error('No password field found');
   });
 
-
-  await passwordFld.fill(password || 'password');
+  await passwordFld.fill(PASSWORD || 'password');
   await passwordFld.press('Enter');
 
-  
-  page.once('dialog', async dialog => {
+  page.once('dialog', async (dialog) => {
     console.log(dialog.message());
     await dialog.accept();
   });
 
-  // get the first applicable reboot button
-  const rebootBtn = await Promise.any([
-    page.getByText('Reboot', { timeout: 1000 }),
-    page.getByText('Reboot Router', { timeout: 1000 }),
-    page.getByText('Reboot Device', { timeout: 1000 }),
-    page.getByText('Reboot Modem', { timeout: 1000 }),
-    page.getByText('Reboot Gateway', { timeout: 1000 }),
-    page.locator(':has-text("Reboot")', { timeout: 1000 })
-  ]).catch(() => {
-    throw new Error('No reboot button found');
-  });
+  // ugly solution for telia-routers
+  // needs refactoring in the future :)
+  const isTeliaRouter = await page.$$('.brandlogin').then((res) => res.length > 0);
+  if (isTeliaRouter) {
+    await page.waitForNavigation({ waitUntil: 'networkidle' });
 
-  await rebootBtn.click();
+    await page.locator('.row >> text="Router"').click();
+    await page.locator('.tabs >> text="Maintenance"').click();
+    await page.locator('.button >> text="Restart"').click();
 
-  // ---------------------
+    const rebootBtn = await page.locator('.button >> text="Reboot"');
+
+    if (!DRYRUN) {
+      await rebootBtn.click();
+    } else {
+      console.log('DRYRUN: Reboot button found!', rebootBtn);
+    }
+  } else {
+    // get the first applicable reboot button
+
+    const rebootBtn = await Promise.any([
+      page.getByText('Reboot', { timeout: 1000 }),
+      page.getByText('Reboot Router', { timeout: 1000 }),
+      page.getByText('Reboot Device', { timeout: 1000 }),
+      page.getByText('Reboot Modem', { timeout: 1000 }),
+      page.getByText('Reboot Gateway', { timeout: 1000 }),
+      page.locator(':has-text("Reboot")', { timeout: 1000 }),
+    ]).catch(() => {
+      throw new Error('No reboot button found');
+    });
+
+    if (!DRYRUN) {
+      await rebootBtn.click();
+    } else {
+      console.log('DRYRUN: Reboot button found!', rebootBtn);
+    }
+  }
+
   await context.close();
 };
 
+// ---------------------
+
 // main program
 (async () => {
-  let config = await getConfig();
-  if (config && process.env) {
-    config = { ...process.env, ...config };
+  let CONFIG = await getConfig();
+  if (CONFIG && process.env) {
+    CONFIG = {
+      ...objectToUpperCase(process.env),
+      ...objectToUpperCase(CONFIG),
+    };
   }
 
   const internetIsReachable = await checkCaptivePortal();
-  if (!internetIsReachable || process.env.IGNORE_CHECK) {
+  if (!internetIsReachable || CONFIG.IGNORE_CHECK) {
     console.log('Internet is not reachable, rebooting device(s)');
 
     const browser = await chromium.launch({
-      headless: process.env.NODE_ENV !== 'development',
+      headless: CONFIG.NODE_ENV !== 'development',
     });
 
-    await rebootDevice({ browser, ...config });
+    await rebootDevice({ browser, ...CONFIG });
 
     await browser.close();
   } else {
